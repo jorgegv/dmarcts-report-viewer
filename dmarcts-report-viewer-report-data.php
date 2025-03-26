3251e323250dd636605a1fcc2ecc6e20c7163f44
Jorge Gonzalez <jorgegv@icij.org>
2025-03-26 18:16:32 +0100

    fix: changed default XML content for NULL raw_xml content in database

--- dmarcts-report-viewer-report-data.php
+++ dmarcts-report-viewer-report-data.php
@@ -59,11 +59,12 @@ function tmpl_reportData($reportnumber, $reports, $host_lookup = 1) {
 		if (empty($row['raw_xml']) ) {
 			$row['raw_xml'] = "
 			<?xml version='1.0' encoding='UTF-8'?>
-			<feedback/>
+			<feedback>
 			<--
 			  The XML source for this report has not been stored in the database
 			  because it exceeded the configured size limit
 			-->
+			</feedback>
 			";
 		}
 
